24e6cf1b1f8ac6e0efc42aabe315ceb97af8c084
Leshoraa <leshoraa@gmail.com>
2026-09-16 15:18:40 +0700

    fix(calc): display result inside calculator under input form without exiting

--- .config/hypr/UserScripts/RofiCalc.sh
+++ .config/hypr/UserScripts/RofiCalc.sh
@@ -8,25 +8,35 @@ if pgrep -x "rofi" >/dev/null; then
     pkill rofi
 fi
 
-user_input=$(rofi -i -dmenu \
-    -config "$rofi_theme" \
-    -p "Calc" </dev/null
-)
+calc_result=""
+last_expr=""
 
-exit_code=$?
-if [ $exit_code -ne 0 ] || [ -z "$user_input" ]; then
-    exit 0
-fi
+while true; do
+    rofi_args=(-i -dmenu -config "$rofi_theme" -p "Calc")
 
-clean_expr=$(echo "$user_input" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-if [ -n "$clean_expr" ]; then
-    raw_result=$(qalc -t "$clean_expr" 2>&1)
-    calc_status=$?
-
-    if [ $calc_status -eq 0 ] && [ -n "$raw_result" ]; then
-        printf "%s" "$raw_result" | wl-copy
-        notify-send -i "accessories-calculator" "Calculator" "$clean_expr = $raw_result" -a "Calculator"
-    else
-        notify-send -u critical -i "dialog-error" "Calculator" "Invalid expression: $clean_expr" -a "Calculator"
+    if [ -n "$calc_result" ]; then
+        rofi_args+=(-mesg "󰃬  <b>$last_expr</b> = <b>$calc_result</b>")
     fi
-fi
+
+    user_input=$(rofi "${rofi_args[@]}" </dev/null)
+    exit_code=$?
+
+    if [ $exit_code -ne 0 ] || [ -z "$user_input" ]; then
+        exit 0
+    fi
+
+    clean_expr=$(echo "$user_input" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    if [ -n "$clean_expr" ]; then
+        raw_result=$(qalc -t "$clean_expr" 2>&1)
+        calc_status=$?
+
+        if [ $calc_status -eq 0 ] && [ -n "$raw_result" ]; then
+            calc_result="$raw_result"
+            last_expr="$clean_expr"
+            printf "%s" "$calc_result" | wl-copy
+        else
+            calc_result="Error"
+            last_expr="$clean_expr"
+        fi
+    fi
+done
